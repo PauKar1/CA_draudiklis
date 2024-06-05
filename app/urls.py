71c750fde802729ce1b9a09fbd86ea17e73602e6
PK <pauliuskaroblis@gmail.com>
2024-06-05 12:42:18 +0300

    updated urls.py discriptions

--- app/urls.py
+++ app/urls.py
@@ -6,52 +6,52 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('price-calculator/', views.price_calculator, name='price_calculator'),
+    path('', views.home, name='home'),  # Pagrindinis puslapis
+    path('price-calculator/', views.price_calculator, name='price_calculator'),  # Draudimo kainos skaičiuoklė
 
     # path('calculate-price/', views.calculate_price, name='calculate_price'),
-    path('app/kliento_paskyra/', views.kliento_paskyra, name='kliento_paskyra'),
-    path('registracija/', views.registracija, name='registracija'),
-    path('app/atsijungti/', views.logout_view, name='logout'),
+    path('app/kliento_paskyra/', views.kliento_paskyra, name='kliento_paskyra'),  # Kliento paskyros peržiūra
+    path('registracija/', views.registracija, name='registracija'),  # Naujo vartotojo registracija
+    path('app/atsijungti/', views.logout_view, name='logout'),  # Vartotojo atsijungimas
 
-    path('password_reset/', auth_view.PasswordResetView.as_view(), name='password_reset'),
-    path('password_reset/done/', auth_view.PasswordResetDoneView.as_view(), name='password_reset_done'),
-    path('reset/<uidb64>/<token>/', auth_view.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-    path('reset/done/', auth_view.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
-    path('client_policies/', views.client_policies, name='client_policies'),
-############# after calculation
-    path('register/', views.register, name='register'),
-    path('register_client/', views.register_client, name='register_client'),
-    path('register_policy/', views.register_policy, name='register_policy'),
-    path('login/', auth_view.LoginView.as_view(template_name='login.html'), name='login'),
-    path('logout/', auth_view.LogoutView.as_view(template_name='logout.html'), name='logout'),
-    path('policy_success/', views.policy_success, name='policy_success'),
+    path('password_reset/', auth_view.PasswordResetView.as_view(), name='password_reset'),  # Slaptažodžio atstatymo užklausos forma
+    path('password_reset/done/', auth_view.PasswordResetDoneView.as_view(), name='password_reset_done'),  # Slaptažodžio atstatymo užklausos išsiuntimas
+    path('reset/<uidb64>/<token>/', auth_view.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),  # Slaptažodžio atstatymo patvirtinimas
+    path('reset/done/', auth_view.PasswordResetCompleteView.as_view(), name='password_reset_complete'),  # Slaptažodžio atstatymo pabaiga
+    path('client_policies/', views.client_policies, name='client_policies'),  # Kliento poliso peržiūra
 
-    path('register_contract/', views.register_contract, name='register_contract'),
-    path('map/', views.map_view, name='map_view'),
-    path('update_klientas/', views.update_klientas, name='update_klientas'),
+    path('register/', views.register, name='register'),  # Vartotojo registracija
+    path('register_client/', views.register_client, name='register_client'),  # Kliento registracija
+    path('register_policy/', views.register_policy, name='register_policy'),  # Poliso registracija
+    path('login/', auth_view.LoginView.as_view(template_name='login.html'), name='login'),  # Vartotojo prisijungimas
+    path('logout/', auth_view.LogoutView.as_view(template_name='logout.html'), name='logout'),  # Vartotojo atsijungimas
+    path('policy_success/', views.policy_success, name='policy_success'),  # Sėkmingai sukurtas polisas
 
-    path('naujas_user_register/', views.naujas_user_register, name='naujas_user_register'),
-    path('choose_account_type/', views.choose_account_type, name='choose_account_type'),
-    path('update_klientas/', views.update_klientas, name='update_klientas'),
-    path('update_brokeris/', views.update_brokeris, name='update_brokeris'),
+    path('register_contract/', views.register_contract, name='register_contract'),  # Kontrakto registracija
+    path('map/', views.map_view, name='map_view'),  # Žemėlapio peržiūra
+    path('update_klientas/', views.update_klientas, name='update_klientas'),  # Kliento informacijos atnaujinimas
 
-    path('profile/edit/', views.profile_view, name='profile_view'),
-    path('profile/', views.profile, name='profile'),
+    path('naujas_user_register/', views.naujas_user_register, name='naujas_user_register'),  # Naujo vartotojo registracija
+    path('choose_account_type/', views.choose_account_type, name='choose_account_type'),  # Pasirinkti paskyros tipą
+    path('update_klientas/', views.update_klientas, name='update_klientas'),  # Kliento informacijos atnaujinimas
+    path('update_brokeris/', views.update_brokeris, name='update_brokeris'),  # Brokerio informacijos atnaujinimas
 
-    path('draudimo-produktai/', views.draudimo_produktai, name='draudimo_produktai'),
-    path('draudimo-produktai/keliones-draudimas/', views.keliones_draudimas, name='keliones_draudimas'),
-    path('faq/', views.faq, name='faq'),
-    path('contact/', views.contact, name='contact'),
+    path('profile/edit/', views.profile_view, name='profile_view'),  # Profilio redagavimas
+    path('profile/', views.profile, name='profile'),  # Profilio peržiūra
 
-    path('partneriams/login/', views.broker_login, name='broker_login'),
-    path('partneriams/register/', views.broker_register, name='broker_register'),
-    path('partneriams/profile/<int:broker_id>/', views.broker_profile, name='broker_profile'),
-    path('partneriams/logout/', views.broker_logout, name='broker_logout'),
+    path('draudimo-produktai/', views.draudimo_produktai, name='draudimo_produktai'),  # Draudimo produktų peržiūra
+    path('draudimo-produktai/keliones-draudimas/', views.keliones_draudimas, name='keliones_draudimas'),  # Kelionių draudimo peržiūra
+    path('faq/', views.faq, name='faq'),  # Dažniausiai užduodami klausimai
+    path('contact/', views.contact, name='contact'),  # Kontaktų forma
 
-    path('broker/register-client-user/', views.broker_klientai_user_create, name='broker_klientai_user_create'),
-    path('broker/update-klientas/<int:client_id>/', views.broker_update_klientas, name='broker_update_klientas'),
+    path('partneriams/login/', views.broker_login, name='broker_login'),  # Brokerio prisijungimas
+    path('partneriams/register/', views.broker_register, name='broker_register'),  # Brokerio registracija
+    path('partneriams/profile/<int:broker_id>/', views.broker_profile, name='broker_profile'),  # Brokerio profilis
+    path('partneriams/logout/', views.broker_logout, name='broker_logout'),  # Brokerio atsijungimas
 
+    path('broker/register-client-user/', views.broker_klientai_user_create, name='broker_klientai_user_create'),  # Brokeris registruoja kliento vartotoją
+    path('broker/update-klientas/<int:client_id>/', views.broker_update_klientas, name='broker_update_klientas'),  # Brokeris atnaujina kliento informaciją
 ]
+
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)  # Medijų failų aptarnavimas debug režime
